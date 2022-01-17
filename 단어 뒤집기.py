bf1c772ae101454553c49773ff09e19d3096e524
YM <zoqxls65456@gmail.com>
2022-01-17 20:23:13 +0900

    단어 뒤집기 초안

--- 단어 뒤집기.py
+++ 단어 뒤집기.py
@@ -2,3 +2,16 @@ import sys
 
 num = int(input())
 
+stack = []
+
+for i in range(num):
+    stack.append([])
+    stack[i] = input().split(" ")
+    
+for j in range(num):
+    for x in range(len(stack[j])):
+        print(stack[j][x][::-1]+" ",end='')
+    if j != num-1:
+        print("")
+
+
