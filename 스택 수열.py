6bed30ec509520eedcf1db448aad68bc0cd7fd1b
YM <zoqxls65456@gmail.com>
2022-01-26 17:51:06 +0900

    스택 수열 수정

--- 스택 수열.py
+++ 스택 수열.py
@@ -7,8 +7,6 @@ N = int(input(""))
 
 for i in range(N):
     list.append(int(input("")))
-
-for i in range(N):
     if next < list[i]:
         for j in range(list[i]-next):
             result.append("+")
@@ -16,14 +14,14 @@ for i in range(N):
             stack.append(next)
         result.append("-")
         stack.pop()
-    elif len(stack) == 0 or (stack[len(stack)-1] < list[i] and list[i] < next):
+    elif len(stack) == 0 or (stack[-1] < list[i] and list[i] < next):
         print("NO")
         break
-    elif list[i] <= stack[len(stack)-1]:
+    elif list[i] <= stack[-1]:
         while(True):
             result.append("-")
             if stack.pop() == list[i]:
                 break
     if i == N-1:
-        for j in range(len(result)):
-            print(result[j])
+        for j in result:
+            print(j)
