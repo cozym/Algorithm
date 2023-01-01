670688f88605c946dad0a886ae6785b9a11de98f
cozym <ddongyee99@daum.net>
2023-01-01 23:54:02 +0900

    first commit in 2023

--- 오등큰수.py
+++ 오등큰수.py
@@ -12,16 +12,9 @@ for i in A:
     else:
         F[i] = 1
 
-#NGF.append(A[0])
 for i in range(N):
         while NGF and F[A[i]] > F[A[NGF[-1]]]:
             result[NGF.pop()] = A[i]
         NGF.append(i)
 
 print(*result)
-
-# [3,3,2,1,1,2,3]
-# 해당 수를 +1
-# 한바퀴 돌려서 각 위치의 빈도수 list추출
-# 두바퀴째 스택을 사용하여 result 업데이트
-# for i in range(N):
---
+++ 후위 표기식2.py
@@ -0,0 +1,5 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip)
+str = list(map(chr,sys.stdin.readline()))
+
