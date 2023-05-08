f2bdd6bceecd2cd670595284bdeb4a42aa28ea6f
cozym <88829892+cozym@users.noreply.github.com>
2023-05-08 23:55:59 +0900

    Timeout code zeroincombi

--- ZeroInCombi.py
+++ ZeroInCombi.py
@@ -1,9 +1,15 @@
-# 조합 0의 개수 2004
 import sys
 
-nm = list(map(int,sys.stdin.readline().split()))
+mn = list(map(int,sys.stdin.readline().split()))
 
-for i in range(n):
-  
+upcount = 0
+downcount = 0
 
-print(nm)
+for i in range(mn[0]-mn[1],mn[0]+1,i*2):
+    upcount += mn[0]//i
+
+for i in range(5,mn[0]-mn[1]+1,i*5):
+    upcount -= 
+    
+
+print(upcount-downcount)
