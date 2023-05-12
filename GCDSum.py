56adba592152aa4bf389b9f7dc21c403643f628e
cozym <ddongyee99@daum.net>
2023-05-13 00:26:18 +0900

    Succeed GCDSum pyVer

--- GCDSum.py
+++ GCDSum.py
@@ -3,9 +3,22 @@ import sys
 
 r = int(sys.stdin.readline())
 arr = []
-print(r)
+
+
+def GCD(a,b):
+    t=0
+    while(a%b!=0):
+        t = b
+        b = a%b
+        a = t
+    return b
+
 
 for i in range(r):
-    arr.append(list(map(int,sys.stdin.readline().split())))
-    
-print(arr)
+    sum = 0
+    arr = list(map(int,sys.stdin.readline().split()))
+    print(arr)
+    for j in range(1,arr[0]):
+        for k in range(j+1,arr[0]+1):
+            sum+=GCD(arr[j],arr[k])
+    print(sum)
