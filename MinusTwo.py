b015c48ed199bf1f68f4bcee85aa126a77cbf805
cozym <ddongyee99@daum.net>
2023-05-23 00:34:31 +0900

    Succeed MinusTwo pyVer

--- MinusTwo.py
+++ MinusTwo.py
@@ -1,5 +1,18 @@
 import sys
 
 r = int(sys.stdin.readline())
+ans = ''
 
-print(r)
+if r==0:
+    print('0')
+    quit()
+
+while r:
+    if r%(-2):
+        ans = '1'+ans
+        r = r//(-2) + 1
+    else:
+        ans = '0'+ans
+        r = r//(-2)
+
+print(ans)
