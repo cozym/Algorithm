fa57ab68775687c79eb3aa3925e6e0ca57b38906
cozym <ddongyee99@daum.net>
2023-05-24 14:03:02 +0900

    Succeed GoldbachPartition pyVer

--- GoldbachPartition.py
+++ GoldbachPartition.py
@@ -1,8 +1,19 @@
 import sys
 
 r = int(sys.stdin.readline())
-evens = []
-for i in range(r):
-    evens.append(int(sys.stdin.readline()))
+#evens = []
+Primes = [True for t in range(1000001)]
 
-print(evens)
+# 에라토스테네스의 채
+for k in range(2,int(1000000**0.5)+1):
+    if Primes[k]:
+        for j in range(k*2, 1000001, k):
+            Primes[j] = False
+
+for i in range(r):
+    N = int(sys.stdin.readline())
+    count = 0
+    for j in range(2,N//2+1):
+        if Primes[j] and Primes[N-j]:
+            count+=1
+    print(count)
