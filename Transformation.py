af7e4951ff9838ee0322f77d0f2037a72d863a86
cozym <ddongyee99@daum.net>
2023-05-30 22:34:33 +0900

    Succeed 2745 pyVer

--- Transformation.py
+++ Transformation.py
@@ -4,17 +4,11 @@ import sys
 n,b = sys.stdin.readline().split()
 ans = 0
 
-for i in n:
-    if i>=65:
-        r = ord(i)-55
+for i,num in enumerate(reversed(n)):
+    if ord(num)>=65:
+        r = int(ord(num)-55)
     else:
-        r = int(i)
-    ans += r
-    ans *= b
+        r = int(num)
+    ans += r*(int(b)**i)
 
-print(b,n)
-
-
-7 // 2 = 3..1
-3 // 2 = 1..1
-1 // 2 = 0..1
+print(ans)
