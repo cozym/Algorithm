bfb6e3d9d41eefdeba8ec3d56bc02103d429fbf9
cozym <ddongyee99@daum.net>
2023-06-12 19:23:47 +0900

    Succeed 11726 pyVer

--- 11726.py
+++ 11726.py
@@ -1,2 +1,11 @@
 # 2*n 타일링
 import sys
+
+n = int(sys.stdin.readline())
+dp = [1,2]
+if n<3:
+    print(dp[n-1])
+else:
+    for i in range(2,n+1):
+        dp.append(dp[i-1]+dp[i-2])
+    print(dp[n-1]%10007)
