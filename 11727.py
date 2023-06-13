05f39df6f043c7f6ca25a4b438bb3e3f0bd3041d
cozym <ddongyee99@daum.net>
2023-06-13 19:31:33 +0900

    Succeed 11727 pyVer

--- 11727.py
+++ 11727.py
@@ -1,1 +1,15 @@
 # 2*n 타일링 2
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0 for i in range(n+1)]
+dp[0] = 1
+dp[1] = 3
+
+if n<3:
+    print(dp[n-1])
+else:
+    for i in range(2,n+1):
+        dp[i] = dp[i-2]*2+dp[i-1]
+    print(dp[n-1]%10007)
