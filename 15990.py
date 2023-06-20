4ab760d0e0428903c29fc0a5d24ea93952b1c663
cozym <ddongyee99@daum.net>
2023-06-20 22:28:21 +0900

    Succeed 15990 pyVer

--- 15990.py
+++ 15990.py
@@ -1,5 +1,21 @@
 # 1,2,3 더하기 5
 import sys
 
-T = int(sys.stdin.readline())
+dp = [[0,0,0] for i in range(100001)]
+dp[0] = [1,0,0]
+dp[1] = [0,1,0]
+dp[2] = [1,1,1]
 
+for i in range(3,100001):
+    dp[i][0] = dp[i-1][1]+dp[i-1][2]%1000000009
+    dp[i][1] = dp[i-2][0]+dp[i-2][2]%1000000009
+    dp[i][2] = dp[i-3][0]+dp[i-3][1]%1000000009
+
+T = int(sys.stdin.readline().rstrip())
+
+for j in range(T):
+    n = int(sys.stdin.readline().rstrip())
+    if n==0:
+        print(0)
+    else:
+        print(sum(dp[n-1])%1000000009)
