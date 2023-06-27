157037634107b2595daa4fc6093ea46b0d0254eb
cozym <ddongyee99@daum.net>
2023-06-27 22:44:53 +0900

    Succeed 11053 pyVer

--- 11053.py
+++ 11053.py
@@ -3,6 +3,13 @@ import sys
 
 N = int(sys.stdin.readline())
 A = list(map(int,sys.stdin.readline().split()))
+dp = [1 for i in range(N)]
 
-print(N)
-print(A)
+# dp[i] = A[i]로 끝나는 LIS
+# if A[j]<A[i]: dp[i] = max(dp[j])+1
+for i in range(N):
+    for j in range(i):
+        if A[j]<A[i] and dp[j]+1>dp[i]:
+            dp[i]=dp[j]+1
+
+print(max(dp))
