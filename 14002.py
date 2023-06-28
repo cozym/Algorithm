f85cdf2c0dc1ca5d2a9d34a856ea772360d020b5
cozym <ddongyee99@daum.net>
2023-06-28 12:26:21 +0900

    Modified 14002

--- 14002.py
+++ 14002.py
@@ -3,5 +3,16 @@ import sys
 
 N = int(sys.stdin.readline())
 A = list(map(int,sys.stdin.readline().split()))
+dp = [1 for i in range(N)]
+dp_list = []
+
+for i in range(N):
+    dp_list.append([A[i]])
+    for j in range(i):
+        if A[j]<A[i] and dp[j]+1>dp[i]:
+            dp[i] = dp[j]+1
+            dp_list[i] = dp_list[j]+dp_list[i]
+print(dp_list)
+
 
 print(N,A)
