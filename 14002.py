0ff565bfefc6840225544cb3bf9a39ad53caedc4
cozym <ddongyee99@daum.net>
2023-06-29 23:46:47 +0900

    Succeed 14002 pyVer

--- 14002.py
+++ 14002.py
@@ -7,12 +7,12 @@ dp = [1 for i in range(N)]
 dp_list = []
 
 for i in range(N):
-    dp_list.append([A[i]])
+    dp_list.append([A[i]])  # dp_list[i]를 A[i]로 초기화하여 default처리
     for j in range(i):
         if A[j]<A[i] and dp[j]+1>dp[i]:
             dp[i] = dp[j]+1
-            dp_list[i] = dp_list[j]+dp_list[i]
-print(dp_list)
+            dp_list[i] = dp_list[j]+[A[i]]  # A[i]로 끝나는 LIS원소들을 dp_list[i]에 초기화
 
-
-print(N,A)
+M = max(dp)
+print(M)
+print(*dp_list[dp.index(M)])    # 최대 길이의 인덱스에 접근
