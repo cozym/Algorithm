ad9d157362f182113fe6973df8eaf782fbd8a0c4
cozym <ddongyee99@daum.net>
2023-07-04 17:55:54 +0900

    Succeed 1912 pyVer

--- 1912.py
+++ 1912.py
@@ -1,1 +1,11 @@
 # 연속합
+import sys
+
+n = int(sys.stdin.readline())
+dp = list(map(int,sys.stdin.readline().split()))  # dp[i] = arr[i]를 끝으로 하는 연속합
+
+for i in range(1,n):
+    if dp[i-1]>0:
+        dp[i] += dp[i-1]
+
+print(max(dp))
