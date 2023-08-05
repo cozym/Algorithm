4246909c671185e652d5975b2b43f6acd825e658
cozym <ddongyee99@daum.net>
2023-08-05 20:59:02 +0900

    Succeed 2225 pyVer

--- 2225.py
+++ 2225.py
@@ -2,12 +2,14 @@
 import sys
 
 N,K = map(int,sys.stdin.readline().split())
-dp = []
+dp = [[0 for i in range(K+1)] for i in range(N+1)]
 
-for i in range(N):
-    for j in range(K):
-        
+# dp[n][k] = sum(dp[n-j][k-1])  0<=j<=n
+for k in range(1,K+1):
+    for n in range(N+1):
+        if k==1:
+            dp[n][k] = 1
+        for i in range(n+1):
+            dp[n][k] = (dp[n][k]+dp[i][k-1])%1000000000
 
-res = 0
-
-print(res)
+print(dp[n][k])
