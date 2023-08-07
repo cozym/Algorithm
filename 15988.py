50149621a65de9cb1d75bdd1cdc3f8e7c325ecd5
cozym <ddongyee99@daum.net>
2023-08-07 19:46:11 +0900

    Succeed 15988 pyVer

--- 15988.py
+++ 15988.py
@@ -1,9 +1,14 @@
 # 1,2,3 더하기 3
 import sys
 
-T = int(sys.stdin.readline())
-n = []
-for i in range(T):
-    n.append(int(sys.stdin.readline()))
+# n[x] = n[x-1]+n[x-2]+n[x-3] / 1,2,3 사전 초기화
+n = [0 for i in range(1000001)]
+n[1] = 1
+n[2] = 2
+n[3] = 4
 
-print(n)
+for j in range(4,1000001):
+    n[j] = (n[j-1]+n[j-2]+n[j-3])%1000000009
+
+for t in range(int(sys.stdin.readline())):
+    print(n[int(sys.stdin.readline())])
