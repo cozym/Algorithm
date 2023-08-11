88f44a1908d9208a87d9699ac5b072cedbf799da
cozym <ddongyee99@daum.net>
2023-08-12 00:21:54 +0900

    Succeed 2156 pyVer

--- 2156.py
+++ 2156.py
@@ -8,11 +8,11 @@ price.append(int(sys.stdin.readline()))
 dp[1][0] = 0
 dp[1][1] = price[0]
 
-#dp[n][0] = dp[n-1][1]
+#dp[n][0] = max(dp[n-1])
 #dp[n][1] = max(dp[n-2][0] + price[n-1] , dp[n-1][0]) + price[n]
 for i in range(2,n+1):
     price.append(int(sys.stdin.readline()))
-    dp[i][0] = dp[i-1][1]
+    dp[i][0] = max(dp[i-1])
     dp[i][1] = max(dp[i-2][0]+price[i-2], dp[i-1][0])+price[i-1]
 
 print(max(dp[n]))
