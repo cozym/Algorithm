8fd35f590eb00f175c6c31dbf8f93a5bfa739c61
cozym <ddongyee99@daum.net>
2023-08-15 22:47:43 +0900

    modified 11722

--- 11055.py
+++ 11055.py
@@ -1,1 +1,15 @@
 # 가장 큰 증가하는 부분 수열
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int,sys.stdin.readline().split()))
+dp = A[:]
+#dp[0] = A[0]
+
+# dp[n] = max(dp[n-i]) + A[n] (0 <= i < n)
+for i in range(N):
+    for j in range(i):
+        if A[j] < A[i] and dp[j]+A[i] > dp[i]:
+            dp[i] = dp[j]+A[i]
+        
+print(dp)
--- 11722.py
+++ 11722.py
@@ -1,1 +1,8 @@
 # 가장 긴 감소하는 부분 수열
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int,sys.stdin.readline().split()))
+dp = [1 for i in range(N)]
+
+# dp[n] = max(dp[n-i])+1    (0<=1<n),(dp[n-i]+1 > dp[n])
