abfc2a307e13d7e7630ccf843418fa2ea210f8cd
cozym <ddongyee99@daum.net>
2023-08-29 23:26:12 +0900

    Succeed 3085 pyVer

--- 3085.py
+++ 3085.py
@@ -28,32 +28,34 @@ def Change_seat(r,c,isrow):
         A[r+1][c] = tmp
 
 # 가로, 세로로 같은 색 최대 개수 카운트
-def Count_max(k,r):
-    count = 1
-    for i in range(1,N):
-        if A[k][i] == A[k][i-1]: # 해당 행의 연속 계산
-            count+=1
-            if res < count: # 최대 길이 저장
-                res = count
-        else:
-            count=1
-    for j in range(1,N):
-        if A[i][r]
+def Count_max(res):
+    for i in range(N):
+        count_r = 1 # 가로 최대 저장
+        count_c = 1 # 세로 최대 저장
+        for j in range(1,N):
+            if A[i][j] == A[i][j-1]: # 해당 행의 연속 계산
+                count_r += 1
+                if res < count_r: # 최대 길이 저장
+                    res = count_r
+            else:
+                count_r = 1
+            if A[j][i] == A[j-1][i]: # 해당 열의 연속 계산
+                count_c += 1
+                if res < count_c: # 최대 길이 저장
+                    res = count_c
+            else:
+                count_c = 1
+    return res
 
-
-
+# 오른쪽,아래쪽 방향으로 원소 교환 O(n**2)
 for k in range(N):
     for r in range(N-1):
-        Change_seat(A[k][r],A[k][r+1],True)
-        #Count_max()
-        Change_seat(A[k][r],A[k][r+1],True)
+        Change_seat(k,r,True)
+        res = Count_max(res)
+        Change_seat(k,r,True)
     for c in range(N-1):
-        Change_seat(A[c][k],A[c+1][k],False)
-        #Count_max()
-        Change_seat(A[c][k],A[c+1][k],False)
+        Change_seat(c,k,False)
+        res = Count_max(res)
+        Change_seat(c,k,False)
 
-
-# for r in range(len(A)):
-#     for e in range(len(A[r])):
-#         print(A[r][e])
-
+print(res)
