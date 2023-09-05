30ce1d4c0ea9e0fa19550c02333cd7e0a87c3d3a
cozym <ddongyee99@daum.net>
2023-09-05 18:04:16 +0900

    modified 6064

--- 6064.py
+++ 6064.py
@@ -5,19 +5,13 @@ T = int(sys.stdin.readline())
 
 for i in range(T):
     M,N,x,y = map(int,sys.stdin.readline().split())
-    for date in range(1,40001):
-        if date%M==0:
-            tmp_x = M
-        else:
-            tmp_x = date%M
+    for date in range(x,40001,M):
         if date%N==0:
             tmp_y = N
         else:
             tmp_y = date%N
-
-        if tmp_x==x and tmp_y==y:
+        if tmp_y==y:
             print(date)
             break
-        if tmp_x==M and tmp_y==N:
-            print(-1)
-            break
+    else:
+        print(-1)
