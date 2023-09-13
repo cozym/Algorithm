4cb9d39de83ba071b01779d0c61717c24be1ad13
cozym <ddongyee99@daum.net>
2023-09-14 02:17:12 +0900

    Succeed 1748 pyVer

--- 1748.py
+++ 1748.py
@@ -2,20 +2,11 @@
 import sys
 
 N = int(sys.stdin.readline())
-d = 10
-res = 0
+i=1
+res=0
 
-# 10부터 10씩 곱해가며 나눴을 때 0이 아니라면 9*(d//10)*(len(d)-1)
-# 나눴을 때 0이라면 (N%(d//10)+1) * (len(d)-1)
-while True:
-    if N<10:
-        res = N
-        break
-    if N//d == 0:
-        res += (N%(d//10)+1)*(len(str(d))-1)
-        break
-    else:
-        res += 9*(d//10)*(len(str(d))-1)
-    d *= 10
+for i in range(1,len(str(N))):
+    res += 9*(10**(i-1))*i
+res += (N - 10**(len(str(N))-1) + 1) * len(str(N))
 
 print(res)
