58e1d88821a1d8d99a2c5f188a8adbf8b0d8fda5
cozym <ddongyee99@daum.net>
2023-09-17 00:53:36 +0900

    Succeed 15652 pyVer

--- 15652.py
+++ 15652.py
@@ -8,14 +8,13 @@ used = [False for i in range(N+1)]
 
 # 재귀func(인덱스,N,M)
 # 인덱스가 M과 같아지면 수열의 길이가 끝나므로 출력
-# 함수로 들어갈때마다 N만큼 반복문을 돌리고 사용되지 않은 수를 used로 체크하여 사용
-def recursion(idx,N,M):
+# start변수를 통해 다음 인덱스에 올 수 있는 가장 작은 값을 제한해준다
+def recursion(idx,N,M,start):
     if idx==M:
-        if p == sorted(p):
-            print(*p)
+        print(*p)
         return
-    for i in range(1,N+1):
+    for i in range(start,N+1):
         p[idx] = i
-        recursion(idx+1,N,M)
+        recursion(idx+1,N,M,i)
 
-recursion(0,N,M)
+recursion(0,N,M,1)
