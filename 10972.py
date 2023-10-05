99870128ff133016f92a3f69ba1ba7cd1988e1ee
cozym <ddongyee99@daum.net>
2023-10-05 21:55:14 +0900

    Modified 10972

--- 10972.py
+++ 10972.py
@@ -1,2 +1,13 @@
 # 다음 순열
 import sys
+import itertools
+
+N = int(sys.stdin.readline())
+compare = list(map(int,sys.stdin.readline().split()))
+
+# itertools를 사용하여 다음 순열 출력
+if compare == sorted(compare,reverse=True):
+    print(-1)
+else:
+    ans = list(itertools.permutations(compare))[1]
+    print(*ans)
