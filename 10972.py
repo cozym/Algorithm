e2c370a22d38f406eca2b77440d7f45c2fde5b0c
cozym <ddongyee99@daum.net>
2023-10-06 22:53:21 +0900

    Succeed 10972 pyVer

--- 10972.py
+++ 10972.py
@@ -3,11 +3,35 @@ import sys
 import itertools
 
 N = int(sys.stdin.readline())
-compare = list(map(int,sys.stdin.readline().split()))
+arr = list(map(int,sys.stdin.readline().split()))
 
-# itertools를 사용하여 다음 순열 출력
-if compare == sorted(compare,reverse=True):
+# 마지막 수열일 경우 -1
+if arr == sorted(arr,reverse=True):
     print(-1)
-else:
-    ans = list(itertools.permutations(compare))[1]
-    print(*ans)
+    exit(0)
+
+line_value = arr[N-1]
+line_index = 0
+swap_idx = 1
+
+# 뒤에서부터 시작하여 오름차순의 마지막 인덱스 i 탐색
+for e in reversed(arr):
+    if line_value > e:
+        break
+    line_value = e
+    line_index += 1
+
+# i~N중 arr[i]보다 큰 가장 작은 수의 인덱스 j를 탐색
+for m in reversed(arr[-line_index:]):
+    if m > arr[-line_index-1]:
+        break
+    swap_idx += 1
+
+# i-1과 j swap
+swap = arr[-line_index-1]
+arr[-line_index-1] = arr[-swap_idx]
+arr[-swap_idx] = swap
+
+# i~N인덱스 뒤집어서 재결합
+arr = arr[:-line_index] + list(reversed(arr[-line_index:]))
+print(*arr)
