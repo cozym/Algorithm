7a449775e2eb84de9ced216b9c604bece9952f44
cozym <ddongyee99@daum.net>
2023-10-19 21:20:35 +0900

    Succeed 14501 pyVer

--- 14501.py
+++ 14501.py
@@ -16,24 +16,25 @@ for i in range(N):
 # i번째 인덱스를 포함할 지 말지 결정
 # 포함 할 경우 i인덱스의 T값을 더해줘서 포인트 점프
 # 포함 안 할 경우 포인터값만 +1
-
-def Resignation(idx):
-    if idx >= N-1:
+def Resignation(idx,s):
+    if idx == N+1:
         global max_sum
+        if max_sum < s:
+            max_sum = s
+        return
+    if idx > N+1:
+        return
+        #global max_sum
         T_sum = 0
         P_sum = 0
-        for i in check:
-            T_sum += T[i]
-            if T_sum > N:
-                break
-            P_sum += P[i]
-        if P_sum > max_sum:
-            max_sum = P_sum
-            print(max_sum)
-        return
-    check.append(idx)
-    Resignation(idx+T[idx])
-    check.pop()
-    Resignation(idx+1)
+        # for i in check:
+        #     P_sum += P[i]
+        # if P_sum > max_sum:
+        #     max_sum = P_sum
+        #     print(max_sum)
+        #return 
+    Resignation(idx+1,s)    
+    Resignation(idx+T[idx-1],s+P[idx-1])   
 
-Resignation(0)
+Resignation(1,0)
+print(max_sum)
