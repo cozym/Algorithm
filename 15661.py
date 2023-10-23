7d755608fcf23f14984e7fb9be3c469708e3d8bb
cozym <ddongyee99@daum.net>
2023-10-23 18:14:47 +0900

    Succeed 15661 pyVer

--- 15661.py
+++ 15661.py
@@ -3,7 +3,8 @@ import sys
 
 N = int(sys.stdin.readline())
 S = []
-part_set = []
+start = []
+link = []
 compare_value = -1
 
 for i in range(N):
@@ -11,28 +12,27 @@ for i in range(N):
 
 # 재귀 부분집합 반복검사
 def start_link(idx, pointer):
-    compare_team(part_set)
-    if pointer >= N:
+    if pointer >= N and (idx >= 1 or idx <= N//2):
+        compare_team(start,link)
         return
-    part_set.append(pointer)
+    start.append(pointer)
     start_link(idx+1,pointer+1)
-    part_set.pop()
+    start.pop()
+    link.append(pointer)
     start_link(idx,pointer+1)
+    link.pop()
 
-# 집합 간 능력치 비교
-def compare_team(part_set):
-    global compare_value     # 개수제한 x 모든 경우 검사
+#집합 간 능력치 비교
+def compare_team(start, link):
+    global compare_value     
     A_team = 0
     B_team = 0
-    for i in range(N):
-        if i in part_set:
-            for j in part_set:  # 팀 A집합 능력치
-                A_team += S[i][j]
-        else:
-            for j in range(N):
-                if j in part_set:# 팀 A의 여집합 능력치
-                    continue
-                B_team += S[i][j]
+    for i in start:
+        for j in start:
+            A_team += S[i][j]
+    for l in link:
+        for m in link:
+            B_team += S[l][m]
     if compare_value == -1:
         compare_value = abs(A_team-B_team)
     if compare_value > abs(A_team-B_team):
