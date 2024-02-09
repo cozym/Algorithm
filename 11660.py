a6565f2896dca4b42b872b77a2905c321a270fcf
cozym <ddongyee99@daum.net>
2024-02-09 23:41:19 +0900

    feat: optimization 11660 pypy3 version and create 1920

--- 11660.py
+++ 11660.py
@@ -2,20 +2,24 @@
 import sys
 
 N,M = map(int, sys.stdin.readline().split())
-table = []
+table = [[0]*(N+1)]
 
-for i in range(N):
-    row = list(map(int,sys.stdin.readline().split()))
-    for i in range(1,N):
+# 이차원 누적합 : 가로 > 세로 누적합
+# 가로 누적합
+for r in range(N):
+    row = [0] + list(map(int,sys.stdin.readline().split()))
+    for i in range(1,N+1):
         row[i] += row[i-1]
     table.append(row)
+
+# 세로 누적합
+for c in range(1,N+1):
+    for i in range(1,N+1):
+        table[i][c] += table[i-1][c]
+
+print(table)
 
 for i in range(M):
     res = 0
     x1,y1,x2,y2 = map(int,sys.stdin.readline().split())
-    for x in range(x1-1,x2):
-        if y1==1:
-            res += table[x][y2-1]
-        else:
-            res += (table[x][y2-1] - table[x][y1-2])
-    print(res)
+    print(table[x2][y2] - table[x1-1][y2] - table[x2][y1-1] + table[x1-1][y1-1])
---
+++ 1920.py
@@ -0,0 +1,10 @@
+# 수 찾기
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int,sys.stdin.readline().split()))
+A.sort()
+
+M = int(sys.stdin.readline())
+ms = list(map(int,sys.stdin.readline().split()))
+
