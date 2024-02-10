88d963ec2f1c8cd0a2577b624d6c854fd9535974
cozym <ddongyee99@daum.net>
2024-02-10 21:52:45 +0900

    refactor: succeed 1920 python3 version

--- 1920.py
+++ 1920.py
@@ -8,3 +8,17 @@ A.sort()
 M = int(sys.stdin.readline())
 ms = list(map(int,sys.stdin.readline().split()))
 
+for m in ms:
+    l,r = 0,len(A)
+    while True:
+        half = (l+r)//2
+        if A[half]==m:
+            print(1)
+            break
+        elif A[half] > m:
+            r = half
+        else:
+            l = half+1
+        if l>=r:
+            print(0)
+            break
