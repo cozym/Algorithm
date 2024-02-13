0419bb3ba4779a1a4cc4a3e7999ef3c6fb6d740b
cozym <ddongyee99@daum.net>
2024-02-13 19:23:34 +0900

    feat: succeed 1072 python3 version

--- 1072.py
+++ 1072.py
@@ -1,1 +1,19 @@
 # 게임
+import sys
+input = sys.stdin.readline
+
+X,Y = map(int,input().split())
+startZ = (Y*100)//X
+
+l,r = 0,X
+game = -1
+while l<=r:
+    h = (l+r)//2
+    currentZ = ((Y+h)*100)//(X+h)
+    if currentZ != startZ:
+        game = h
+        r = h-1
+    else:
+        l = h+1
+
+print(game)
