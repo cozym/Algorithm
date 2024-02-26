fa652cf5309eeff335073a0fdb4c0c72fcc05b16
cozym <ddongyee99@daum.net>
2024-02-26 23:57:51 +0900

    refactor: modified 2468

--- 2468.py
+++ 2468.py
@@ -1,1 +1,14 @@
 # 안전 영역
+import sys
+input = sys.stdin.readline
+
+cnt = 0
+area = []
+visit = []
+
+N = int(input())
+for _ in range(N):
+    area.append(list(map(int,input().strip().split())))
+    visit.append([False]*N)
+
+print(visit)
