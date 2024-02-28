6f5970e88a74a5aff4ad8ca81ec2c6acd0dd0d0d
cozym <ddongyee99@daum.net>
2024-02-29 02:14:54 +0900

    feat: succeed 1991 python3 version

--- 1991.py
+++ 1991.py
@@ -1,2 +1,37 @@
 # 트리 순회
 import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+tree = {}
+
+for _ in range(int(input())):
+    parent,left,right = map(str,input().split())
+    tree[parent] = [left,right]
+
+def preOrder(node):
+    print(node,end='')
+    if tree[node][0] != '.':
+        preOrder(tree[node][0])
+    if tree[node][1] != '.':
+        preOrder(tree[node][1])
+
+def inOrder(node):
+    if tree[node][0] != '.':
+        inOrder(tree[node][0])
+    print(node,end='')
+    if tree[node][1] != '.':
+        inOrder(tree[node][1])
+
+def postOrder(node):
+    if tree[node][0] != '.':
+        postOrder(tree[node][0])
+    if tree[node][1] != '.':
+        postOrder(tree[node][1])
+    print(node,end='')
+
+preOrder('A')
+print()
+inOrder('A')
+print()
+postOrder('A')
