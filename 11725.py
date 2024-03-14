8e3b394f32a6dffbd9e3ddfaf6c28479657fe0a4
cozym <ddongyee99@daum.net>
2024-03-14 22:47:04 +0900

    feat: succeed 11725 python3 version

--- 11725.py
+++ 11725.py
@@ -1,1 +1,28 @@
 # 트리의 부모 찾기
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+R = [[] for _ in range(N+1)]
+res = [0 for _ in range(N+1)]
+Q = deque()
+
+for _ in range(N-1):
+    l,r = map(int,input().split())
+    R[l].append(r)
+    R[r].append(l)
+
+for node in R[1]:
+    res[node] = 1
+    Q.append(node)
+
+while len(Q) != 0:
+    current = Q.popleft()
+    for node in R[current]:
+        if res[node] == 0:
+            res[node] = current
+            Q.append(node)
+
+for i in range(2,N+1):
+    print(res[i])
