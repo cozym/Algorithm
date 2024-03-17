6b832c9e42babacd9be676495304eb0a450db3a8
cozym <ddongyee99@daum.net>
2024-03-17 23:36:49 +0900

    feat: succeed 15681 python3 version

--- 15681.py
+++ 15681.py
@@ -1,6 +1,7 @@
 # 트리와 쿼리
 import sys
 from collections import deque
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 N,R,Q = map(int,input().split())
@@ -8,12 +9,15 @@ Rel = [[] for _ in range(N+1)]
 Tree = [[] for _ in range(N+1)]
 visit = [False]*(N+1)
 q = deque()
+subTree = [0 for _ in range(N+1)]
 
+# 노드 인접리스트 생성
 for _ in range(N-1):
     n1,n2 = map(int,input().split())
     Rel[n1].append(n2)
     Rel[n2].append(n1)
 
+# BFS로 트리의 부모자식 관계 형성
 q.append(R)
 visit[R] = True
 while len(q) != 0:
@@ -24,14 +28,18 @@ while len(q) != 0:
             visit[node] = True
             q.append(node)
 
+# DFS와 DP로 서브트리의 정점 수 저장
+def DFS(treeNode):
+    for node in Tree[treeNode]:
+        if subTree[node] == 0:
+            DFS(node)
+        subTree[treeNode] += subTree[node]
+    subTree[treeNode] += 1
+
+for idx in range(1,N+1):
+    if subTree[idx] == 0:
+        DFS(idx)
+
+# 출력
 for _ in range(Q):
-    cnt = 0
-    #visit = [False]*(N+1)
-    q.clear()
-    q.append(int(input()))
-    while len(q) != 0:
-        current = q.popleft()
-        for node in Tree[current]:
-            q.append(node)
-            cnt += 1
-    print(cnt+1)
+    print(subTree[int(input())])
