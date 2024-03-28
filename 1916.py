22b14ea24ced870bd233faff1faedd72c61fa1f0
cozym <ddongyee99@daum.net>
2024-03-28 23:45:01 +0900

    feat: succeed 1916 python3 version

--- 1916.py
+++ 1916.py
@@ -1,1 +1,33 @@
 # 최소비용 구하기
+import sys
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+N = int(input())
+R = [[] for _ in range(N+1)]
+distance = [INF for _ in range(N+1)]
+Q = []
+# M = int(input())
+for _ in range(int(input())):
+    s,d,c = map(int,input().split())
+    R[s].append([c,d])
+
+start, reach = map(int,input().split())
+
+distance[start] = 0
+heapq.heappush(Q,[0,start])
+while Q:
+    nowCost, nowNode = heapq.heappop(Q)
+
+    if distance[nowNode] < nowCost:
+        continue
+    
+    for node in R[nowNode]:
+        nextCost, nextNode = node[0], node[1]
+        if nowCost + nextCost < distance[nextNode]:
+            distance[nextNode] = nowCost + nextCost
+            heapq.heappush(Q,[nowCost + nextCost,nextNode])
+
+
+print(distance[reach])
