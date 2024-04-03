f7ed42b707e6133f972cbdc0121ce0de5e3286ed
cozym <ddongyee99@daum.net>
2024-04-03 17:05:12 +0900

    feat: succeed 18223 python3 version

--- 18223.py
+++ 18223.py
@@ -1,1 +1,38 @@
 # 민준이와 마산 그리고 건우
+import sys
+from heapq import heappush,heappop
+input = sys.stdin.readline
+INF = int(1e9)
+
+V,E,P = map(int,input().split())
+R = [[] for _ in range(V+1)]
+
+def dijkstra(start):
+    distance = [INF for _ in range(V+1)]
+    distance[start] = 0
+    Q = []
+    heappush(Q,[0,start])
+
+    while Q:
+        nowCost, nowNode = heappop(Q)
+        if distance[nowNode] < nowCost:
+            continue
+        for nextCost,nextNode in R[nowNode]:
+            if nowCost+nextCost < distance[nextNode]:
+                distance[nextNode] = nowCost+nextCost
+                heappush(Q,[nowCost+nextCost,nextNode])
+    
+    return distance
+
+for _ in range(E):
+    a,b,c = map(int,input().split())
+    R[a].append([c,b])
+    R[b].append([c,a])
+
+distanceFrom1 = dijkstra(1)
+distanceFromP = dijkstra(P)
+
+if distanceFromP[1]+distanceFromP[V] <= distanceFrom1[V]:
+    print("SAVE HIM")
+else:
+    print("GOOD BYE")
