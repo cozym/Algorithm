1130924373dfe1a3390a145b409e0a24f89a4d63
cozym <ddongyee99@daum.net>
2024-04-08 19:31:52 +0900

    feat: succeed 14888 python3 version

---
+++ 14888.py
@@ -0,0 +1,29 @@
+# 연산자 끼워넣기
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+operators = list(map(int,input().split()))
+maxRes = -1e9
+minRes = 1e9
+
+def searchMaxMin(idx, n, add, sub, mul, div):
+    if idx == N:
+        global maxRes,minRes
+        maxRes = max(maxRes,n)
+        minRes = min(minRes,n)
+        return
+    
+    if add:
+        searchMaxMin(idx+1, n+A[idx], add-1, sub, mul, div)
+    if sub:
+        searchMaxMin(idx+1, n-A[idx], add, sub-1, mul, div)
+    if mul:
+        searchMaxMin(idx+1, n*A[idx], add, sub, mul-1, div)
+    if div:
+        searchMaxMin(idx+1, int(n/A[idx]), add, sub, mul, div-1)
+    
+searchMaxMin(1,A[0],operators[0],operators[1],operators[2],operators[3])
+print(maxRes)
+print(minRes)
