01f6a289ffde20c6894469f5e768b01bcc9c3b6a
cozym <ddongyee99@daum.net>
2024-04-16 20:10:58 +0900

    feat: succeed 24594 python3 version

--- 24954.py
+++ 24954.py
@@ -1,1 +1,36 @@
 # 물약 구매
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+
+N = int(input())
+C = list(map(int,input().split()))
+p = {}
+res = 1e9
+
+for i in range(1,N+1):
+    p[i] = []
+    for j in range(int(input())):
+        a,d = map(int,input().split())
+        p[i].append([a,d])
+
+def calculate(l,c):
+    priceAll = 0
+    global res
+    for d in l:
+        priceAll += c[d-1]
+        if priceAll > res:
+            return
+        for m in p[d]:
+            c[m[0]-1] -= m[1]
+            if c[m[0]-1] < 1:
+                c[m[0]-1] = 1
+    if priceAll < res:
+        res = priceAll
+
+check = permutations(range(1,N+1),N)
+
+for l in list(check):
+    calculate(l,C[:])
+
+print(res)
