9ad5aa8d8b01f00ad2fc272ee6c07b8a5b71c7ac
cozym <ddongyee99@daum.net>
2024-04-24 23:32:41 +0900

    refactor: 2309.py diff version

--- 2309.py
+++ 2309.py
@@ -1,23 +1,43 @@
 # 일곱 난쟁이
+# import sys
+
+# heights = []
+# for i in range(9):
+#     heights.append(int(sys.stdin.readline()))
+# res = heights[:]
+# twosum = sum(heights)-100
+
+# # 다 더하고 2개씩 묶어서 빼봤을 때 100
+# for i in range(8):
+#     for j in range(i+1,9):
+#         if heights[i]+heights[j]==twosum:
+#             res.remove(heights[i])
+#             res.remove(heights[j])
+#             break
+#     if len(res)!=9:
+#         break
+
+# res.sort()
+
+# for p in res:
+#     print(p)
+
 import sys
+from itertools import combinations
+input = sys.stdin.readline
 
 heights = []
-for i in range(9):
-    heights.append(int(sys.stdin.readline()))
-res = heights[:]
-twosum = sum(heights)-100
+for _ in range(9):
+    heights.append(int(input()))
 
-# 다 더하고 2개씩 묶어서 빼봤을 때 100
-for i in range(8):
-    for j in range(i+1,9):
-        if heights[i]+heights[j]==twosum:
-            res.remove(heights[i])
-            res.remove(heights[j])
-            break
-    if len(res)!=9:
+s = sum(heights)
+pickTwo = combinations(heights,2)
+for a,b in list(pickTwo):
+    if s-(a+b) == 100:
+        heights.remove(a)
+        heights.remove(b)
         break
+heights.sort()
 
-res.sort()
-
-for p in res:
-    print(p)
+for h in heights:
+    print(h)
