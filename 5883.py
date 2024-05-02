cc90b7549580fca38fbcdaba2b826f1310ccdef6
cozym <ddongyee99@daum.net>
2024-05-02 17:32:12 +0900

    feat: succeed 5883.py

--- 5883.py
+++ 5883.py
@@ -1,2 +1,25 @@
 # 아이폰 9S
 import sys
+input = sys.stdin.readline
+
+N = int(input())
+capacity = []
+for _ in range(N):
+    capacity.append(int(input()))
+kinds = set(capacity)
+res = 0
+
+for target in kinds:
+    sequence = 0
+    pre = -1
+    for i in capacity:
+        if i==target:
+            continue
+        if i != pre:
+            pre = i
+            sequence = 1
+        else:
+            sequence += 1
+        res = max(sequence, res)
+
+print(res)
