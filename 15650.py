182d1c9c1466bbf0fca5742ce4d782a6e44661f6
cozym <ddongyee99@daum.net>
2024-05-03 14:53:13 +0900

    feat: refactor 15650.py

--- 15650.py
+++ 15650.py
@@ -3,23 +3,20 @@ import sys
 
 N,M = map(int,sys.stdin.readline().split())
 
-p = [0 for i in range(M)]
-used = [False for i in range(N+1)]
+pick = [0 for _ in range(M)]
+used = [False for _ in range(N+1)]
 
-# 재귀func(인덱스,N,M)
-# 인덱스가 M과 같아지면 수열의 길이가 끝나므로 출력
-# 함수로 들어갈때마다 N만큼 반복문을 돌리고 사용되지 않은 수를 used로 체크하여 사용
-def recursion(idx,N,M):
+def recursion(idx,start):
     if idx==M:
-        if p==sorted(p):
-            print(*p)
+        print(*pick)
         return
-    for i in range(1,N+1):
+    
+    for i in range(start,N+1):
         if used[i]:
             continue
+        pick[idx] = i
         used[i] = True
-        p[idx] = i
-        recursion(idx+1,N,M)
+        recursion(idx+1,i+1)
         used[i] = False
 
-recursion(0,N,M)
+recursion(0,1)
