b84ed93ec123d57891b89c37da882d0b92acbb6a
cozym <ddongyee99@daum.net>
2024-05-17 11:46:57 +0900

    feat: succeed swea1289.py

--- 15486.py
+++ 15486.py
@@ -1,1 +1,23 @@
 # 퇴사 2
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000000)
+
+N = int(input())
+coun = []
+for _ in range(N):
+    t,p = map(int,input().split())
+    coun.append((t,p))
+dp = [-1]*N
+
+def recur(idx):
+    if idx > N:
+        return -1e9
+    if idx == N:
+        return 0
+    if dp[idx] != -1:
+        return dp[idx]
+    dp[idx] = max(recur(idx+1),recur(idx+coun[idx][0])+coun[idx][1])
+    return dp[idx]
+
+print(recur(0))
