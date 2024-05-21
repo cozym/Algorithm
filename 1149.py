1e2e6683e324a75f85806469d63d6b8b308f3a00
YM <zoqxls65456@gmail.com>
2024-05-21 16:30:57 +0900

    feat: succeed 12865.py

--- 1149.py
+++ 1149.py
@@ -11,8 +11,8 @@ dp[0] = RGB[0].copy()
 # 점화식 dp[i][r] = min(dp[i-1][g]+rgb[i][r] , dp[i-1][b]+rgb[i][r])
 for i in range(1,N):
     RGB.append(list(map(int,sys.stdin.readline().split())))
-    dp[i][0] = min(dp[i-1][1]+RGB[i][0], dp[i-1][2]+RGB[i][0])
-    dp[i][1] = min(dp[i-1][0]+RGB[i][1], dp[i-1][2]+RGB[i][1])
-    dp[i][2] = min(dp[i-1][0]+RGB[i][2], dp[i-1][1]+RGB[i][2])
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2])+RGB[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2])+RGB[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1])+RGB[i][2]
 
 print(min(dp[N-1]))
--- 12865.py
+++ 12865.py
@@ -1,1 +1,23 @@
 # 평범한 배낭
+import sys
+input = sys.stdin.readline
+
+N, K = map(int,input().split())
+bag = []
+for _ in range(N):
+    w,v = map(int,input().split())
+    bag.append((w,v))
+
+dp = [[-1]*100001 for _ in range(N)]
+
+def recur(idx, weight):
+    if weight > K:
+        return -10000000000
+    if idx == N:
+        return 0
+    if dp[idx][weight] != -1:
+        return dp[idx][weight]
+    dp[idx][weight] = max(recur(idx+1, weight), recur(idx+1, weight+bag[idx][0])+bag[idx][1])
+    return dp[idx][weight]
+
+print(recur(0,0))
