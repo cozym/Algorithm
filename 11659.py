e8513bc185ddc5c62f586a74799128cf381a85fb
cozym <ddongyee99@daum.net>
2024-05-29 16:20:05 +0900

    feat: succeed 11659.py

---
+++ 11659.py
@@ -0,0 +1,16 @@
+# 구간 합 구하기 4
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+nums = list(map(int,input().split()))
+prefixSum = []
+prefixSum.append(0)
+for n in range(1,N+1):
+    prefixSum.append(prefixSum[n-1]+nums[n-1])
+
+print(nums,prefixSum)
+
+for _ in range(M):
+    i,j = map(int,input().split())
+    print(prefixSum[j]-prefixSum[i-1])
