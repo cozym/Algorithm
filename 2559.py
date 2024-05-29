710eb3657868b2c19e860d58b50a9186e1d95a20
cozym <ddongyee99@daum.net>
2024-05-29 16:33:58 +0900

    feat: succeed 2559.py prefixSum version

--- 2559.py
+++ 2559.py
@@ -4,10 +4,23 @@ input = sys.stdin.readline
 
 N,K = map(int,input().split())
 temps = list(map(int,input().split()))
+res = 0
 
-sequence_sum = []
-sequence_sum.append(sum(temps[:K]))
-for i in range(1,N-K+1):
-    sequence_sum.append(sequence_sum[i-1]-temps[i-1]+temps[i+K-1])
+## 슬라이딩 윈도우
+# sequence_sum = []
+# sequence_sum.append(sum(temps[:K]))
+# for i in range(1,N-K+1):
+#     sequence_sum.append(sequence_sum[i-1]-temps[i-1]+temps[i+K-1])
 
-print(max(sequence_sum))
+# print(max(sequence_sum))
+
+
+## 누적합
+prefixSum = [0]*(N+1) 
+for i in range(1,N+1):
+    prefixSum[i] = prefixSum[i-1]+temps[i-1]
+
+for idx in range(K,N+1):
+    res = max(res, prefixSum[idx]-prefixSum[idx-K])
+
+print(res)
