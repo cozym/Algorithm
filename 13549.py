3b283e84f016a9a15a27f3c526a4bccfdd29f459
cozym <ddongyee99@daum.net>
2024-06-19 22:22:49 +0900

    feat: succeed 13649.py

---
+++ 13549.py
@@ -0,0 +1,36 @@
+# 숨바꼭질 3
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,K = map(int,input().split())
+point = [0]*100001
+
+def bfs(x,k):
+    q = deque()
+    if x == 0:
+        q.append(1)
+    else:
+        q.append(x)
+    
+    while q:
+        x = q.popleft()
+        if x==k:
+            return point[x]
+
+        for nx in (x*2,x+1,x-1):
+            if 0 <= nx <= 100000 and point[nx]==0:
+                if nx == 2*x:
+                    point[nx] = point[x]
+                    q.appendleft(nx)
+                else:
+                    point[nx] = point[x]+1
+                    q.append(nx)
+
+if N==0:
+    if K==0:
+        print(0)
+    else:
+        print(bfs(N,K)+1)
+else :
+    print(bfs(N,K))
